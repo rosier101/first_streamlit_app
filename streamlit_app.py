e0c0110abcd70a486acca496abd93c7592d42c52
Rose <rosier101@gmail.com>
2022-05-09 16:17:08 -0400

    Update streamlit_app.py
    We want to filter the table data based on the fruits a customer will choose, so we'll pre-populate the list to set an example for the customer.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 #add a user interactive widget called multi-select that will allow users to pick the fruits they want in their smoothies
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avovado', 'Strawberries'])
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
