a6d7bf86e086300d83287c6b0ebcb826d27f91cc
Rose <rosier101@gmail.com>
2022-05-13 16:48:24 -0400

    Update streamlit_app.py
    changed variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #add a second input entry
-fruit_choice2 = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write('Thanks for adding', fruit_choice2)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
+streamlit.write('Thanks for adding', add_my_fruit)
