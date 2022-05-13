2acdaa8b757a60a698cb2cc204648283e462f2d0
Rose <rosier101@gmail.com>
2022-05-13 17:07:17 -0400

    Update streamlit_app.py
    fixing indent again

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,14 +38,14 @@ try:
    if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
    else:
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-       #take the json version of the response and normalize it
-       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       #output it the screen as a table
-       streamlit.dataframe(fruityvice_normalized)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        #take the json version of the response and normalize it
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        #output it the screen as a table
+        streamlit.dataframe(fruityvice_normalized)
 
- except URLError as e:
-     streamlit.error()
+except URLError as e:
+    streamlit.error()
 
     #adding stop command. dont' run anything past here while we trouble shoot
 streamlit.stop()
